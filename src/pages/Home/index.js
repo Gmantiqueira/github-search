1713fb679ed494062a46e54b413cb7fac3bdff86
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-05-31 19:33:56 -0300

    feature WIP | Finishing Homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import useDebounce from '@/hooks/useDebounce';
+import api from '@/services/api';
 
-import { Container } from './styles';
+import Card from '@/components/Card';
+import Loader from '@/components/Loader';
+
+import { Container, InputWrapper, SearchInput, UsersList } from './styles';
 
 function Home() {
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [hasTyped, triggerTyped] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    const debounceSearch = useDebounce(search, 1000);
+
+    const getUsers = useCallback(async () => {
+        try {
+            const { data } = await api.get('search/users', {
+                params: { q: search },
+            });
+
+            setUsers(data.items);
+            setLoading(false);
+        } catch (err) {
+            setLoading(false);
+        }
+    }, [search]);
+
+    function searchHandler(value) {
+        triggerTyped(true);
+        // setSearch(value);
+    }
+
+    useEffect(() => {
+        if (debounceSearch) {
+            getUsers();
+        } else {
+            setLoading(false);
+            setUsers([]);
+        }
+    }, [debounceSearch, getUsers]);
+
     return (
-        <Container>
-            <h1>Home</h1>
+        <Container className="bg-gray-dark">
+            <InputWrapper isSearching={hasTyped}>
+                <SearchInput
+                    type="text"
+                    name="search"
+                    onKeyUp={(e) => searchHandler(e.target.value)}
+                />
+            </InputWrapper>
+            <UsersList>
+                <Loader />
+            </UsersList>
         </Container>
     );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,51 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+import { $grayDark } from '@/styles/colors';
+
+export const Container = styled.main`
     align-items: center;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+    padding: 0 16px;
+
+    @media screen and (min-width: 992px) {
+        padding: 0 64px;
+    }
+`;
+
+export const InputWrapper = styled.div`
+    align-items: flex-end;
+    display: flex;
+    height: ${({ isSearching }) => (isSearching ? '208px' : '100%')};
+
+    transition: height ease-out 0.5s;
+`;
+
+export const SearchInput = styled.input`
+    border-radius: 8px;
+    max-width: 360px;
+    width: 100%;
+
+    margin: auto;
+    padding: 8px 24px;
+
+    color: ${$grayDark()};
+
+    font-size: 24px;
+
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        color: ${$grayDark(50)};
+    }
+    :-ms-input-placeholder {
+        color: ${$grayDark(50)};
+    }
+`;
+
+export const UsersList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 100%;
 `;
