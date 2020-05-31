e94f95cc3bddaab3cb8d8f7325529a0bbd8e9430
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-05-31 19:33:35 -0300

    feature | Creating threeDots Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Wrapper } from './styles';
+
+function Loader() {
+    return (
+        <Wrapper>
+            <div className="dot1" />
+            <div className="dot2" />
+            <div className="dot3" />
+        </Wrapper>
+    );
+}
+
+export default Loader;
