817743b2d1fd773fd4c11e777569dadb717f781d
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-06-02 01:07:21 -0300

    feature WIP | Handling data prop in DataCount component

--- src/components/DataCount/index.js
+++ src/components/DataCount/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import numFormatter from '@/utils/numFormatter';
 
 import { CountWrapper } from './styles';
 
 function DataCount({ label, data }) {
+    const dataFormat = data
+        ? typeof data === 'number' || parseInt(data)
+            ? numFormatter(data)
+            : data
+        : 'N/A';
+
     return (
         <CountWrapper>
             <h3 className="text-gray-dark center">{label}</h3>
-            <h2 className="text-blue center regular">{data || 'N/A'}</h2>
+            <h2 className="text-blue center regular">{dataFormat}</h2>
         </CountWrapper>
     );
 }
+
+DataCount.propTypes = {
+    label: PropTypes.string.isRequired,
+    data: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
 
 export default DataCount;
