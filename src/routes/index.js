d172a9b6b18d1cccdf2ef23ba5f0252bed2055c2
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-06-02 01:06:13 -0300

    feature | Creating Error component and 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Error404 from '../pages/404';
 import Home from '../pages/Home';
 import User from '../pages/User';
 
@@ -8,7 +9,8 @@ export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/users/:user" exact component={User} />
+            <Route path="/:username/:reponame?" exact component={User} />
+            <Route component={Error404} />
         </Switch>
     );
 }
