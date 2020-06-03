e88d0f19cacfabfebb87eaae5a22843cf4f081c1
Gabriel Antiqueira <gantiqueira@neoassist.com>
2020-06-03 17:10:31 -0300

    bugfix | Removing link from DataCount component

--- src/components/DataCount/index.js
+++ src/components/DataCount/index.js
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types';
 
 import { CountWrapper } from './styles';
 
-function DataCount({ label, data, link }) {
+function DataCount({ label, data }) {
     return (
         <CountWrapper>
             <h3 className="text-gray-dark center">{label}</h3>
-            {link ? (
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                    <h3 className="text-blue center regular">{data}</h3>
-                </a>
-            ) : (
-                <h3 className="text-blue center regular">{data}</h3>
-            )}
+            <h3 className="text-blue center regular">{data}</h3>
         </CountWrapper>
     );
 }
 
 DataCount.propTypes = {
     label: PropTypes.string.isRequired,
-    data: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    link: PropTypes.string,
+    data: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 DataCount.defaultProps = {
-    link: '',
+    data: '',
 };
 
 export default DataCount;
