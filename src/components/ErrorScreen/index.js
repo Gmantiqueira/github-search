388b70be98efb920c50a5c763d70b0b9491c7242
Gabriel Antiqueira <gantiqueira@neoassist.com>
2020-06-04 10:54:20 -0300

    bugfix | Adding propTypes

--- src/components/ErrorScreen/index.js
+++ src/components/ErrorScreen/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 
 function ErrorScreen({ children }) {
     return <Container>{children}</Container>;
+}
+
+ErrorScreen.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired
 }
 
 export default ErrorScreen;
